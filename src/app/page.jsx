b00180ac02d1c82730449b297de8e6c8e3384220
Code-With-Devansh <95998107+Code-With-Devansh@users.noreply.github.com>
2026-01-25 18:09:42 +0530

    homepage links added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
-
+import { WEBSITE_LOGIN, WEBSITE_REGISTER } from '@/routes/WebsiteRoute'
 const page = () => {
   return (
     <div>
-      <Button>Click me</Button>
+      <Link href={WEBSITE_LOGIN}>register</Link>
+      <Link href={WEBSITE_REGISTER}>login</Link>
     </div>
   )
 }
